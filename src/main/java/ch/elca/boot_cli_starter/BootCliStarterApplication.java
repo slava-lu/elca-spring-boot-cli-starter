39167131bc9df88d743b6365c4dbb515280c48d6
lusv <viacheslav.lushchinskiy@elca.ch>
2024-11-10 18:06:58 +0100

    initial commit

---
+++ src/main/java/ch/elca/boot_cli_starter/BootCliStarterApplication.java
@@ -0,0 +1,13 @@
+package ch.elca.boot_cli_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCliStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCliStarterApplication.class, args);
+	}
+
+}
